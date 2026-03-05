ed28eac3036de716738f6b248d45836011ba3ed8
Fullgamer494 <fullgamer494@hotmail.com>
2026-03-05 14:00:04 -0600

    feat: añadir yml para que heroku dockerice

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const allowedOrigins = ['http://localhost:3000'];
+if (process.env.FRONTEND_URL) {
+    allowedOrigins.push(process.env.FRONTEND_URL);
+}
+
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: function(origin, callback){
+       if(!origin) return callback(null, true);
+       if(allowedOrigins.indexOf(origin) === -1){
+         var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+         return callback(new Error(msg), false);
+       }
+       return callback(null, true);
+    }
 }));
 app.use(express.json());
 
